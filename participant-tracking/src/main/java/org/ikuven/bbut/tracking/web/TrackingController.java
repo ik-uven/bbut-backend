ef09794ddb34ebc30171440f4cdc51383315b0ea
Ken Alexandersson <ken.alexandersson@jeppesen.com>
2020-05-24 11:40:10 +0200

    revamped sorting

--- participant-tracking/src/main/java/org/ikuven/bbut/tracking/web/TrackingController.java
+++ participant-tracking/src/main/java/org/ikuven/bbut/tracking/web/TrackingController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping(path = "/api")
 public class TrackingController {
 
     private final ParticipantService participantService;
