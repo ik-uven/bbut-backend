5a079ab212896a796b98b27539136e51474e71bf
Ken Alexandersson <ken.alexandersson@gmail.com>
2020-05-24 17:03:18 +0200

    Corrected ordinal in ParticipantState (affects the sorting, no-show should be last)

--- participant-tracking/src/main/java/org/ikuven/bbut/tracking/participant/ParticipantState.java
+++ participant-tracking/src/main/java/org/ikuven/bbut/tracking/participant/ParticipantState.java
@@ -2,8 +2,8 @@ package org.ikuven.bbut.tracking.participant;
 
 public enum ParticipantState {
 
-    REGISTERED,
-    NO_SHOW,
     ACTIVE,
-    RESIGNED
+    RESIGNED,
+    REGISTERED,
+    NO_SHOW
 }
