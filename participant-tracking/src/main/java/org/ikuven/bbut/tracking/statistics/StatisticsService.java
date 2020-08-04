f6b2783ed5352a5de90acd98bdeb8e6ebca03d37
Ken Alexandersson <ken.alexandersson@gmail.com>
2020-08-04 16:14:11 +0200

    Demographics statistics now shows all statuses except noshow

--- participant-tracking/src/main/java/org/ikuven/bbut/tracking/statistics/StatisticsService.java
+++ participant-tracking/src/main/java/org/ikuven/bbut/tracking/statistics/StatisticsService.java
@@ -12,6 +12,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static java.util.function.Predicate.not;
+
 @Slf4j
 @Component
 public class StatisticsService {
@@ -48,7 +50,9 @@ public class StatisticsService {
         int UPPER_LIMIT = 79;
         List<String> keys = List.of("0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+");
 
-        return participantService.getActivatedParticipants().stream()
+        return participantService.getAllParticipants().stream()
+                .filter(Objects::nonNull)
+                .filter(not(participant -> participant.getParticipantState().equals(ParticipantState.NO_SHOW)))
                 .map(participant -> calculateAge(participant.getBirthDate()))
                 .filter(Objects::nonNull)
                 .map(age -> Math.min(age, UPPER_LIMIT) / 10)
@@ -64,8 +68,9 @@ public class StatisticsService {
     }
 
     public Map<ParticipantClass, Long> getClassDemographics() {
-        return participantService.getActivatedParticipants().stream()
+        return participantService.getAllParticipants().stream()
                 .filter(Objects::nonNull)
+                .filter(not(participant -> participant.getParticipantState().equals(ParticipantState.NO_SHOW)))
                 .filter(participant -> Objects.nonNull(participant.getParticipantClass()))
                 .collect(Collectors.groupingBy(Participant::getParticipantClass, Collectors.counting()));
     }
